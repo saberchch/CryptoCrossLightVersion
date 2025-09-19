74b3af6cd57cdc08ae6d02e21efca2eba78707cf
saberchch <saberchaabani94@gmail.com>
2025-09-19 20:45:48 +0200

    quiz dashboard b

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -3,19 +3,14 @@ import React from 'react';
 interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
   header?: React.ReactNode;
   footer?: React.ReactNode;
-  as?: keyof JSX.IntrinsicElements;
 }
 
-export default function Card({ header, footer, as: Tag = 'div', className = '', children, ...rest }: CardProps) {
+export default function Card({ header, footer, className = '', children, ...rest }: CardProps) {
   return (
-    <Tag className={`card ${className}`} {...rest}>
-      {header ? <div className="card-header">{header}</div> : null}
-      <div>
-        {children}
-      </div>
-      {footer ? <div className="card-footer">{footer}</div> : null}
-    </Tag>
+    <div className={`card ${className}`} {...rest}>
+      {header && <div className="card-header">{header}</div>}
+      <div>{children}</div>
+      {footer && <div className="card-footer">{footer}</div>}
+    </div>
   );
 }
-
-
